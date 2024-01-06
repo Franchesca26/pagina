fb84b830c0e6af1bbfd9da689f7af2ca8846df2b
Franchesca <franchescaros26@gmail.com>
2024-01-05 21:12:50 -0400

    Tienda

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const btnCart = document.querySelector('.container-icon')
-
+const btnCart = document.querySelector('.container-cart-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart')
 })
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product')
+const rowProduct = document.querySelector('.row-productor')
+
+//Lista de todos los contenedores de los productos 
+const productsList =document.querySelector('.container-items') 
+
+//variable de arreglos de productos 
+let allproducts = []
+
+
+
+
+productsList.addEventListener('click', e => {
+
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElement
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector
+        }
+    }
+
+})
+
